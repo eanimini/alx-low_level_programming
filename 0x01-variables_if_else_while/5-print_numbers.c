fbe172919719f4b3efb84755532861b5397e3c0f
eanimini <minio.afework@gmail.com>
2022-06-06 19:02:04 -0700

    prints single digit numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,11 @@
  */
 int main(void)
 {
-	int num;
-	for (num = 0; num < 10; num++);
-		printf(num);
-		return (0)
+int num;
+
+for (num = 0; num < 10; num++);
+	{
+	printf(num);
+	}
+	return (0);
 }
