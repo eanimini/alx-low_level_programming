2a8f3cb35d81df4f135c87f2b565cc2fd82017f3
eanimini <minio.afework@gmail.com>
2022-06-06 18:52:53 -0700

    prints single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++);
+		printf(num);
+		return (0)
+}
