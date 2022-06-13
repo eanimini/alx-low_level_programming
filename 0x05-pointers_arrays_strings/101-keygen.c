e2d6442c3cb3ef950e53b6faef74163a1af05f52
eanimini <minio.afework@gmail.com>
2022-06-13 13:47:05 -0700

    a program that generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,51 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <time.h>
 
-void randomPasswordGeneration(int N)
+/**
+  * main - generates random passwords for 101-crackme
+  * Return: zero
+  */
+int main(void)
 {
-	int i = 0;
-	int randomizer = 0;
+	int sum;
+	char c;
 
-	srand((unsigned int)(time(NULL)));
-	char numbers[] = "0123456789";
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	char symbols[] = "!@#$^&*?";
-	char password[N];
-
-	randomizer = rand() % 4;
-
-	for (i = 0; i < N; i++) 
+	srand(time(NULL));
+	while (sum <= 2645)
 	{
-	if (randomizer == 1) 
-	{
-		password[i] = numbers[rand() % 10];
-		randomizer = rand() % 4;
-		printf("%c", password[i]);
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
-
-int main()
-{
-    int N = 10;
- 
-    randomPasswordGeneration(N);
-  
-    return 0;
+	putchar(2772 - sum);
+	return (0);
 }
