19c7d4c219fe448568d945cd2b63a5c5c18362e6
eanimini <minio.afework@gmail.com>
2022-06-13 14:07:49 -0700

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,38 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 
 /**
-* print_number - prints # using _putchar function
-* @n: the integer to print
-*
-* Return: void
-*/
+ * print_number - like a hello world
+ *
+ * @n: params an integer
+ *
+ * No return
+ */
+
+
 void print_number(int n)
 {
-	int copy, nth, size = 1, ones = n % 10;
 
-	n /= 10;
-	copy = n;
-	if (ones < 0)
+	unsigned int n1 = 0;
+
+	if  (n < 0)
 	{
-		ones *= -1, copy *= -1, n *= -1;
+		n1 = -n;
 		_putchar('-');
 	}
-	if (copy > 0)
+
+	else
 	{
-		while (copy / 10 != 0)
-		{
-			copy /= 10, size *= 10;
-		}
-		while (size > 0)
-		{
-			nth = n / size;
-			_putchar('0' + nth);
-			n -= nth * size;
-			size /= 10;
-		}
+		n1 = n;
 	}
-	_putchar('0' + ones);
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
