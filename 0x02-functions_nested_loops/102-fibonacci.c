67da73373815a881401b87977fbb56b3a69dda67
eanimini <minio.afework@gmail.com>
2022-06-13 14:32:12 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,5 +7,19 @@
 */
 int main(void)
 {
-int a, b;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+
+	printf("%lu", bef);
+
+	for (i = 1; i < 50; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	printf("\n");
+	return (0);
+}
 
