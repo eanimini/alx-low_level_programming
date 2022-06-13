74c541fecd8e832a006a1e780e354d9b545ebac9
eanimini <minio.afework@gmail.com>
2022-06-13 13:37:50 -0700

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,46 +1,28 @@
 #include <stdlib.h>
 #include "main.h"
-// Function to randomly generates password
-// of length N
+
 void randomPasswordGeneration(int N)
 {
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
-    char numbers[] = "0123456789";
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
+	int i = 0;
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[N];
+
+	randomizer = rand() % 4;
+
+	for (i = 0; i < N; i++) 
+	{
+	if (randomizer == 1) 
+	{
+		password[i] = numbers[rand() % 10];
+		randomizer = rand() % 4;
+		printf("%c", password[i]);
+	}
         else if (randomizer == 2) {
             password[i] = symbols[rand() % 8];
             randomizer = rand() % 4;
@@ -58,15 +40,11 @@ void randomPasswordGeneration(int N)
         }
     }
 }
-  
-// Driver Code
+
 int main()
 {
-    // Length of the password to
-    // be generated
     int N = 10;
-  
-    // Function Call
+ 
     randomPasswordGeneration(N);
   
     return 0;
